3571b24c3c3ed4c18883b96e228d104acbf35bac
Faixxx <lolfaisal631@gmail.com>
2025-08-05 09:48:18 +0700

    reroute

--- static/gnunesia/js/articles.js
+++ static/gnunesia/js/articles.js
@@ -4,7 +4,7 @@ const articles = [
     judul: "Mengapa Linux Menjadi Pilihan Para Developer",
     tanggal: "2025-07-23",
     penulis: "Tim Linux Freedom",
-    gambar: "static/assets/img/the_linux_kernel.jpg",
+    gambar: "/static/assets/img/the_linux_kernel.jpg",
     konten: `
       <p>Linux adalah sistem operasi open source yang digunakan oleh jutaan developer di seluruh dunia...</p>
       <p>Dengan fleksibilitas dan keamanan yang ditawarkannya, Linux menjadi landasan bagi server, sistem embedded, dan bahkan desktop sehari-hari.</p>
@@ -15,7 +15,7 @@ const articles = [
     judul: "Kebebasan Digital dalam Dunia Open Source",
     tanggal: "2025-07-22",
     penulis: "Faixxx GPT",
-    gambar: "static/assets/img/linux-dev.jpg",
+    gambar: "/static/assets/img/linux-dev.jpg",
     konten: `
       <p>Open source tidak hanya soal kode terbuka. Ia adalah simbol perlawanan terhadap monopoli dan sensor digital...</p>
       <p>Dalam artikel ini, kita akan menggali lebih dalam filosofi kebebasan digital yang dibawa oleh proyek-proyek open source seperti GNU/Linux.</p>
